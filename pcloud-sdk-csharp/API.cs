3197853bbc1e9b6f4eba1309cd2142605937b2ff
nico.borsdorf <nico.borsdorf@siemens.com>
2024-05-28 14:55:22 +0200

    added sharing controller

--- pcloud-sdk-csharp/API.cs
+++ pcloud-sdk-csharp/API.cs
@@ -1,6 +1,8 @@
-﻿using pcloud_sdk_csharp.File.Controller;
+﻿using pcloud_sdk_csharp.Archiving.Controller;
+using pcloud_sdk_csharp.File.Controller;
 using pcloud_sdk_csharp.Folder.Controller;
 using pcloud_sdk_csharp.Streaming.Controller;
+using pcloud_sdk_csharp.Sharing.Controller;
 
 namespace pcloud_sdk_csharp.Client
 {
@@ -14,6 +16,8 @@ namespace pcloud_sdk_csharp.Client
             Folders = new(token, baseURL);
             Files = new(token, baseURL);
             Streaming = new(token, baseURL);
+            Archiving = new(token, baseURL);
+            Sharing = new(token, baseURL);
         }
 
         public PCloudClient(string token, string? clientURL = null)
@@ -24,10 +28,14 @@ namespace pcloud_sdk_csharp.Client
             Folders = new(token, baseURL);
             Files = new(token, baseURL);
             Streaming = new(token, baseURL);
+            Archiving = new(token, baseURL);
+            Sharing = new(token, baseURL);
         }
 
         public FolderController Folders { get; private set; }
         public FileController Files { get; private set; }
         public StreamingController Streaming { get; private set; }
+        public ArchivingController Archiving { get; private set; }
+        public SharingController Sharing { get; private set; }
     }
 }
