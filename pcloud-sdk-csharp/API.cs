a67c742c2464808f5833fb3ccc9f9c19cae0f934
nico.borsdorf <nico.borsdorf@siemens.com>
2024-05-28 10:12:59 +0200

    restructure base

---
+++ pcloud-sdk-csharp/API.cs
@@ -0,0 +1,33 @@
+﻿using pcloud_sdk_csharp.File.Controller;
+using pcloud_sdk_csharp.Folder.Controller;
+using pcloud_sdk_csharp.Streaming.Controller;
+
+namespace pcloud_sdk_csharp.Client
+{
+    public class PCloudClient
+    {
+        public PCloudClient(string token, Uri? clientURL = null)
+        {
+            if (token == null) throw new ArgumentNullException(nameof(token));
+            string baseURL = clientURL?.ToString() ?? new Uri("https://eapi.pcloud.com/").ToString();
+
+            Folders = new(token, baseURL);
+            Files = new(token, baseURL);
+            Streaming = new(token, baseURL);
+        }
+
+        public PCloudClient(string token, string? clientURL = null)
+        {
+            if (token == null) throw new ArgumentNullException(nameof(token));
+            string baseURL = clientURL ?? new Uri("https://eapi.pcloud.com/").ToString();
+
+            Folders = new(token, baseURL);
+            Files = new(token, baseURL);
+            Streaming = new(token, baseURL);
+        }
+
+        public FolderController Folders { get; private set; }
+        public FileController Files { get; private set; }
+        public StreamingController Streaming { get; private set; }
+    }
+}
--- pcloud-sdk-csharp/Models.cs
+++ pcloud-sdk-csharp/Models.cs
@@ -1,211 +1,71 @@
 ﻿using System.Text.Json;
 
-namespace pcloud_sdk_csharp.Requests
+
+namespace pcloud_sdk_csharp.Base.Requests
 {
-
-    public class AuthorizeRequest : BaseRequest
+    public class Tree
     {
-        public AuthorizeRequest(string client_Id, ResponseType responseType, string? redirectUri = null, string? state = null, bool? forceApprove = false)
-        {
-            if (responseType == ResponseType.token && redirectUri == null) throw new ArgumentNullException(nameof(redirectUri));
-
-            Client_Id = client_Id;
-            Type = responseType;
-            RedirectUri = redirectUri;
-            State = state;
-            ForceApprove = forceApprove ?? false;
-        }
-
-        public string Client_Id { get; set; } = null!;
-        public ResponseType Type { get; set; }
-        public string? RedirectUri { get; set; }
-        public string? State { get; set; }
-        public bool ForceApprove { get; set; }
-
-        public enum ResponseType
-        {
-            code = 0,
-            token = 1
-        }
-    }
-
-    public class ListFolderRequest : BaseRequest
-    {
-        public ListFolderRequest(long folderId, bool recursive = false, bool showDeleted = false, bool noFiles = false, bool noShares = false)
-        {
-            FolderId = folderId;
-            Recursive = recursive ? 1 : 0;
-            ShowDeleted = showDeleted ? 1 : 0;
-            NoFiles = noFiles ? 1 : 0;
-            NoShares = noShares ? 1 : 0;
-        }
-
-        public long FolderId { get; set; }
-        public int Recursive { get; set; }
-        public int ShowDeleted { get; set; }
-        public int NoFiles { get; set; }
-        public int NoShares { get; set; }
-
-    }
-
-    public class RenameFolderRequest : BaseRequest
-    {
-        public RenameFolderRequest(long folderId, long? toFolderId, string toName)
-        {
-            FolderId = folderId;
-            ToFolderId = toFolderId ?? folderId;
-            ToName = toName;
-        }
-
-        public long FolderId { get; set; }
-        public long ToFolderId { get; set; }
-        public string? ToName { get; set; }
-    }
-
-    public class CopyFolderRequest : BaseRequest
-    {
-        public CopyFolderRequest(long folderId, long toFolderId, bool noOver = false, bool skipExisting = false, bool copyContentOnly = false)
-        {
-            FolderId = folderId;
-            ToFolderId = toFolderId;
-            NoOver = noOver ? 1 : 0;
-            SkipExisting = skipExisting ? 1 : 0;
-            CopyContentOnly = copyContentOnly ? 1 : 0;
-        }
-
-        public long FolderId { get; set; }
-        public long ToFolderId { get; set; }
-        public int NoOver { get; set; }
-        public int SkipExisting { get; set; }
-        public int CopyContentOnly { get; set; }
-    }
-
-    public class UploadFileRequest : BaseRequest
-    {
-        public UploadFileRequest(long folderId, string fileName, Stream uploadFile, string? progressHash = null, bool noPartial = true, bool renameIfExists = true)
-        {
-            FolderId = folderId;
-            FileName = fileName;
-            UploadFile = uploadFile;
-            ProgressHash = progressHash;
-            RenameIfExists = renameIfExists ? 1 : 0;
-            NoPartial = noPartial ? 1 : 0;
-        }
-
-        public long FolderId { get; set; }
-        public string FileName { get; set; }
-        public Stream UploadFile { get; set; }
-        public string? ProgressHash { get; set; }
-        public int NoPartial { get; set; }
-        public int RenameIfExists { get; set; }
-    }
-
-    public class BaseRequest
-    {
-        public string ToJson()
-        {
-            return JsonSerializer.Serialize(this);
-        }
+        public List<long> FolderIds { get; } = new List<long> { };
+        public List<long> FileIds { get; } = new List<long> { };
+        public List<long> ExcludeFolderIds { get; } = new List<long> { };
+        public List<long> ExcludeFileIds { get; } = new List<long> { };
     }
 }
 
 // response properties are written in small letters due to json mapping of ws reponse
-namespace pcloud_sdk_csharp.Responses
+namespace pcloud_sdk_csharp.Base.Responses
 {
-    // Auth
-    public class AuthResponse : Response
-    {
-        public string code { get; set; } = null!;
-        public string access_token { get; set; } = null!;
-        public string token_type { get; set; } = null!;
-        public string state { get; set; } = null!;
-        public int locationid { get; set; }
-        public int uid { get; set; }
-        public string hostname { get; set; } = null!;
-    }
-
-
-    // Files
-    public class UploadedFile : Response
-    {
-        public List<long> fileids { get; set; } = null!;
-        public List<Metadata> metadata { get; set; } = null!;
-    }
-
-    public class UploadProgress
-    {
-        public long total { get; set; }
-        public long uploaded { get; set; }
-        public string currentfile { get; set; } = null!;
-        public List<Metadata> files { get; set; } = null!;
-        public bool finished { get; set; }
-    }
-
-    // Folders
-    public class Folder : Response
-    {
-        public bool created { get; set; }
-        public string id { get; set; } = null!;
-        public Metadata metadata { get; set; } = null!;
-    }
-
-    public class DeleteFolder : Response
-    {
-        public int deletedfiles { get; set; }
-        public int deletedfolders { get; set; }
-    }
-
     public class Response
     {
-        public int result { get; set; }
-        public string? error { get; set; }
+        public int result { get; protected set; }
+        public string? error { get; protected set; }
     }
 
     // Metadata and all super categories
     public class Metadata
     {
-        public long parentfolderid { get; set; }
-        public long? fileid { get; set; }
-        public long? folderid { get; set; }
-        public bool isfolder { get; set; }
-        //public bool ismine { get; set; }
-        //public bool isshared { get; set; }
-        public string name { get; set; } = null!;
-        //public string id { get; set; }
-        //public string? deletedfileid { get; set; }
-        public string created { get; set; } = null!;
-        public string modified { get; set; } = null!;
-        //public string icon { get; set; }
-        //public long category { get; set; }
-        //public bool thumb { get; set; }
-        public long size { get; set; }
-        //public string contenttype { get; set; }
-        //public long hash { get; set; }
-        public List<Metadata>? contents { get; set; }
-        //public bool isdeleted { get; set; }
-        //public string path { get; set; }
+        public long parentfolderid { get; protected set; }
+        public long? fileid { get; protected set; }
+        public long? folderid { get; protected set; }
+        public bool isfolder { get; protected set; }
+        public bool ismine { get; protected set; }
+        public bool isshared { get; protected set; }
+        public string name { get; protected set; } = null!;
+        public string id { get; protected set; } = null!;
+        public string? deletedfileid { get; protected set; }
+        public DateTime created { get; protected set; }
+        public DateTime modified { get; protected set; }
+        public string? icon { get; protected set; }
+        public long category { get; protected set; }
+        public bool thumb { get; protected set; }
+        public long size { get; protected set; }
+        public string? contenttype { get; protected set; }
+        public long hash { get; protected set; }
+        public List<Metadata>? contents { get; protected set; }
+        public bool isdeleted { get; protected set; }
+        public string? path { get; protected set; }
 
         // For image files
-        //public int? width { get; set; } -- see video
-        //public int? weight { get; set; }
+        //public int? width { get; set; } --> see video
+        public long? weight { get; protected set; }
 
         // For Audio files
-        //public string? artist { get; set; }
-        //public string? album { get; set; }
-        //public string? title { get; set; }
-        //public string? genre { get; set; }
-        //public string? trackno { get; set; }
+        public string? artist { get; protected set; }
+        public string? album { get; protected set; }
+        public string? title { get; protected set; }
+        public string? genre { get; protected set; }
+        public string? trackno { get; protected set; }
 
-        //// For video files
-        //public int? width { get; set; }
-        //public int? height { get; set; }
-        //public float? duration { get; set; }
-        //public float? fps { get; set; }
-        //public string? videocodec { get; set; }
-        //public string? audiocodec { get; set; }
-        //public int? videobitrate { get; set; }
-        //public int? audiobitrate { get; set; }
-        //public int? audiosamplerate { get; set; }
-        //public int? rotate { get; set; }
+        // For video files
+        public long? width { get; protected set; }
+        public long? height { get; protected set; }
+        public float? duration { get; protected set; }
+        public float? fps { get; protected set; }
+        public string? videocodec { get; protected set; }
+        public string? audiocodec { get; protected set; }
+        public long? videobitrate { get; protected set; }
+        public long? audiobitrate { get; protected set; }
+        public long? audiosamplerate { get; protected set; }
+        public long? rotate { get; protected set; }
     }
 }
