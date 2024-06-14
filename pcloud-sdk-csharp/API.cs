1fdf8e241b22d11dea38c4a2b3985abbc7bc06a8
nico.borsdorf <nico.borsdorf@siemens.com>
2024-06-14 13:56:17 +0200

    collection

--- pcloud-sdk-csharp/API.cs
+++ pcloud-sdk-csharp/API.cs
@@ -8,23 +8,26 @@ using pcloud_sdk_csharp.Thumbnails.Controller;
 using pcloud_sdk_csharp.UploadLinks.Controller;
 using pcloud_sdk_csharp.Revisions.Controller;
 using pcloud_sdk_csharp.Trash.Controller;
+using pcloud_sdk_csharp.Collection.Controller;
 
 namespace pcloud_sdk_csharp.Client
 {
     public class PCloudClient
     {
         /// <summary>
-        /// Create instance of PCloud client which connects to the API.
-        /// The different endpoints can be used with the corresponding controller.
+        /// Create instance of PCloud client which connects to the API. <br/>
+        /// The different endpoints can be used with the corresponding controller.<br/>
+        /// Only Auth required controllers are used in the Client. Others can be initiated outside of is.
         /// </summary>
         /// <param name="token">Access token for API connection</param>
-        /// <param name = "clientURL" > API URL depending on region, either https://api.plcoud.com/ or default https://eapi.plcoud.com/ in case if no URL is provided.</param>
+        /// <param name = "clientURL" > API URL depending on region, either https://api.plcoud.com/ or default https://eapi.plcoud.com/ in case if no URL is provided. / at the end of the URL is required.</param>
         /// <exception cref="ArgumentNullException">
         /// When <paramref name="token"/> is null.
         /// </exception>
         public PCloudClient(string token, Uri? clientURL = null)
         {
             if (token == null) throw new ArgumentNullException(nameof(token));
+            if (clientURL != null && !clientURL.AbsoluteUri.Last().Equals("/)")) throw new Exception(@"Please append a / to the clientURL. E.g. https://eapi.pcloud.com/");
             if (clientURL == null) clientURL = new Uri("https://eapi.pcloud.com/");
 
             Folders = new(token, clientURL);
@@ -37,7 +40,9 @@ namespace pcloud_sdk_csharp.Client
             UploadLinks = new(token, clientURL);
             Revisions = new(token, clientURL);
             Trash = new(token, clientURL);
+            Collection = new(token, clientURL);
         }
+
 
         public FolderController Folders { get; private set; }
         public FileController Files { get; private set; }
@@ -49,5 +54,6 @@ namespace pcloud_sdk_csharp.Client
         public UploadLinksController UploadLinks { get; private set; }
         public RevisionsController Revisions { get; private set; }
         public TrashController Trash { get; private set; }
+        public CollectionController Collection { get; private set; }
     }
 }
