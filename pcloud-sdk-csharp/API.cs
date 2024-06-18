085f03c52b2f433b65085e690fbd5da96e779907
nico.borsdorf <nico.borsdorf@siemens.com>
2024-06-18 13:30:45 +0200

    check

--- pcloud-sdk-csharp/API.cs
+++ pcloud-sdk-csharp/API.cs
@@ -27,7 +27,7 @@ namespace pcloud_sdk_csharp.Client
         public PCloudClient(string token, Uri? clientURL = null)
         {
             if (token == null) throw new ArgumentNullException(nameof(token));
-            if (clientURL != null && !clientURL.AbsoluteUri.Last().Equals(char.Parse("/"))) throw new Exception("Please append a / to the clientURL. E.g. https://eapi.pcloud.com/");
+            if (clientURL != null && clientURL.AbsoluteUri.Last().Equals(char.Parse("/")) == false) throw new Exception("Please append a / to the clientURL. E.g. https://eapi.pcloud.com/");
             if (clientURL == null) clientURL = new Uri("https://eapi.pcloud.com/");
 
             Folders = new(token, clientURL);
